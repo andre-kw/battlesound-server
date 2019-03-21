e72d18144aea8a5a2cbef80905370d83a5331beb
Andre K. W <andre.k.willie@gmail.com>
2019-03-21 17:02:47 -0400

    added some columns

--- migrations/001.do.create_contests.sql
+++ migrations/001.do.create_contests.sql
@@ -3,5 +3,8 @@ CREATE TABLE contests (
   title TEXT NOT NULL,
   creator INTEGER NOT NULL,
   date_created TIMESTAMP DEFAULT now() NOT NULL,
-  date_ending TIMESTAMP NOT NULL
+  date_ending TIMESTAMP NOT NULL,
+  max_submissions INTEGER NOT NULL,
+  max_participants INTEGER NOT NULL,
+  official BOOLEAN DEFAULT FALSE NOT NULL
 );
